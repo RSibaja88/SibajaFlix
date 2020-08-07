c8774021d112906c7284e35a5fe464bae998ec67
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-08-07 01:26:58 -0400

    api fetching data

--- sibajaflix/src/helpers/requests.js
+++ sibajaflix/src/helpers/requests.js
@@ -1,8 +1,8 @@
 const API_KEY = "b04f1d6f68a9f349441ebfd89f4e45d0";
 
 const request = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchTopRated: `/movies/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
